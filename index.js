09861697fe059c7523a9a9c83be1ddf1fb78f245
krishna-nishant <nishantkrishna2005@gmail.com>
2025-01-21 22:46:16 +0530

    feat: rename main.js to index.js and add concurrently and CORS support

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import questionRoutes from './routes/route.js';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", questionRoutes);

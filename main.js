5f07c388ee2cdbf9e3d9d0701b41d3d3c7a28639
krishna-nishant <nishantkrishna2005@gmail.com>
2025-01-21 15:13:14 +0530

    refactor: restructure API routes and move question handling to a separate module

--- main.js
+++ main.js
@@ -1,26 +1,14 @@
 import express from 'express';
-import client from './client.js';
 import dotenv from 'dotenv';
-dotenv.config();
+import questionRoutes from './routes/route.js';
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    client.getQuestions({}, (err, data) => {
-        if (err) {
-            res.status(500).send({
-                msg: "gRPC error",
-                details: err.message,
-            });
-            console.error(err);
-        } else {
-            res.send(data);
-        }
-    });
-});
+app.use("/api", questionRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Express server is running on port ${process.env.PORT}`);
---
+++ routes/route.js
@@ -0,0 +1,20 @@
+import client from '../client.js'
+import express from 'express'
+
+const router = express.Router()
+
+router.get('/questions', (req, res) => {
+    client.getQuestions({}, (err, data) => {
+        if (err) {
+            res.status(500).send({
+                msg: "gRPC error",
+                details: err.message,
+            });
+            console.error(err);
+        } else {
+            res.send(data);
+        }
+    });
+});
+
+export default router;

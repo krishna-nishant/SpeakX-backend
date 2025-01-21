83df8ce30a7e471a5a5ba7fbc9dbd1789ccef5f9
krishna-nishant <nishantkrishna2005@gmail.com>
2025-01-21 09:49:51 +0530

    fix: update MongoDB connection URI for database access

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,19 +1,19 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config(); // Load environment variables
+dotenv.config(); 
 
 const dbUsername = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
 
-const mongoURI = `mongodb+srv://${dbUsername}:${dbPassword}@speakx.ai9ip.mongodb.net/speakxquestions`
+const mongoURI = `mongodb+srv://${dbUsername}:${dbPassword}@speakx.y2ih0.mongodb.net/speakxquestions`
 const connectDB = async () => {
     try {
         await mongoose.connect(mongoURI)
         console.log('Connected to MongoDB');
     } catch (err) {
         console.error('MongoDB connection error:', err);
-        process.exit(1); // Exit process with failure
+        process.exit(1);
     }
 };
 
